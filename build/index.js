97059d9a4f610fdc7e0f877909cbfc4896d0d7f9
liushaozhen <804199994@qq.com>
2020-03-13 10:37:38 +0800

    publish 0.0.1

---
+++ build/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _CnCorecss = require('./CnCorecss');
+
+var _CnCorecss2 = _interopRequireDefault(_CnCorecss);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+exports["default"] = _CnCorecss2["default"];
+module.exports = exports['default'];
